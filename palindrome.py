8fc21406ae18f14ea5a949f965e50dda97942dae
Some Guy <abc@abc.com>
2015-10-06 14:31:52 -0700

    HW #2 Palindrome

---
+++ palindrome.py
@@ -0,0 +1,25 @@
+import re
+
+
+user_sentence = input("Please enter your sentence > ")
+
+stripped_sentence = re.sub('[^A-Za-z]', '', user_sentence).lower()
+
+backwards = []
+count = 0
+count_back = -1
+
+while count < len(stripped_sentence):
+    backwards.append(stripped_sentence[count_back])
+    count_back -= 1
+    count += 1
+
+backwards_sentence = "".join(backwards)
+
+print(stripped_sentence)
+print(backwards_sentence)
+
+if stripped_sentence == backwards_sentence:
+    print("Yes it is a palindrome!")
+else:
+    print("No it is not a palindrome!")
