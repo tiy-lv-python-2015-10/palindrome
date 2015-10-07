76b0c6b0603f828b66436972e6299585639b82e7
cesarmarroquin <cesarm2333@gmail.com>
2015-10-07 08:56:12 -0700

    both programs run back to back now

--- palindrome.py
+++ palindrome.py
@@ -1,6 +1,6 @@
 #ITERATIVE PROGRAM
 import re
-def main_program():
+def iterative_program():
     """main function which has the core program (should be broken into smaller functions)"""
     user_input = re.sub( r'[^A-Za-z]', '', str(input("Please enter some text to check if it is a palindrome: ")))
     user_input_length = len(user_input)
@@ -16,12 +16,10 @@ def main_program():
     theoretical_math_piece = ((str(str(re.match(str(user_input), str(user_input_reverse)))[24:34])) + " )")#the match reg ex function is applied to both the actual text and the reversed text and it compares the two and returns a math object based off of the result# after i recieve the math object i take out the span part of the object which looks similar to this <_sre.SRE_Match object; span=(4, 11), match='message'>
     actual_math_piece = ("span=(0, "+str(user_input_length)+" )")#this is the span part that should be generated if text is palandromic
     print("thats a palindrome!") if actual_math_piece == theoretical_math_piece else print("nope that is not a palindrome!")
-main_program()
+iterative_program()
 
 #RECURSIVE PROGRAM
-'''
-import re
-def main_program():
+def recrursive_program():
     """main function which has the core program"""
     counter = 0
     user_input = re.sub( r'[^A-Za-z]', '', str(input("Please enter some text to check if it is a palindrome: ")))
@@ -49,5 +47,4 @@ def main_program():
         print("thats a palindrome!") if actual_math_piece == theoretical_math_piece else print("nope that is not a palindrome!")
 
     reverse_string(counter, user_input, user_input_reverse, user_input_length)
-main_program()
-'''
+recrursive_program()
